1d2d258f703014cbbfe5fcece30e35b369551685
xuwenyong <Ack@xuwenyongdeMacBook-Pro.local>
2015-12-01 16:34:31 +0800

    Separate Arrays+Strings to two type of projects, Add project for bit manipulation.

---
+++ BitManipulation/BitManipulation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BitManipulation
+//
+//  Created by xuwenyong on 15/11/23.
+//  Copyright © 2015年 Ack. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ Strings/README.md
@@ -0,0 +1,2 @@
+Operations about Strings.
+
